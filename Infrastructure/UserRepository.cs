7c0220153a22e8a1156e7d712086889f7375d888
Kiran Gondi <kiran.gondi@gmail.com>
2024-04-29 21:48:01 +0530

    UserRepo added
    UserRepo added

---
+++ Infrastructure/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using CQRSwithMediatoR.Domain;
+
+namespace CQRSwithMediatoR.Infrastructure
+{
+  public class UserRepository
+  {
+    private readonly List<User> _users = new List<User>();
+    
+    public User AddUser(User user)
+    {
+      ColorConsole.WriteLineRepository($"Adding user with name.'{user.Name}'");
+      var currentMaxId = !_users.Any() ? 0 : _users.Max(u => u.Id);
+      user.Id = currentMaxId + 1;
+      _users.Add(user);
+      return user;
+    }
+
+    public int GetUserCount(string? name)
+    {
+      ColorConsole.WriteLineRepository($"Filtering users where name is '{name}'");
+      return _users == null ? _users.Count : _users.Count(u => u.Name.Contains(name));
+    }
+
+    public void DeleteAll()
+    {
+      ColorConsole.WriteLineRepository($"Deleting all users.");
+      _users.Clear();
+    }
+  }
+}
