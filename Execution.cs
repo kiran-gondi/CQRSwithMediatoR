97125d5a570b889292493da6ae2f2bee1f7d70ac
Kiran Gondi <kiran.gondi@gmail.com>
2024-04-29 21:30:10 +0530

    First Cut
    First Cut

---
+++ Execution.cs
@@ -0,0 +1,42 @@
+﻿using CQRSwithMediatoR.Command;
+using CQRSwithMediatoR.Query;
+using MediatR;
+
+namespace CQRSwithMediatoR
+{
+  public class Execution
+  {
+    private readonly IMediator _mediator;
+    public Execution(IMediator mediator)
+    {
+      _mediator = mediator;
+    }
+
+    public async Task RunAsync()
+    {
+      ColorConsole.WriteLineProgram("Creating a new user with name 'Steven'");
+      var user0 = await _mediator.Send(new AddNewUserCommand { Name = "Steven" });
+      ColorConsole.WriteLineProgram($"User has id: {user0.Id}");
+
+      Console.WriteLine();
+
+      ColorConsole.WriteLineProgram("Creating a new user with name 'Rebecca'");
+      var user1 = await _mediator.Send(new AddNewUserCommand { Name = "Rebecca" });
+      ColorConsole.WriteLineProgram($"User has id: {user1.Id}");
+
+      Console.WriteLine();
+
+      ColorConsole.WriteLineProgram($"Get count of users with name 'Rebecca'");
+      var count = await _mediator.Send(new GetUserCountQuery { NameFilter = "Rebecca" });
+      ColorConsole.WriteLineProgram($"Found {count} entries");
+
+      Console.WriteLine();
+      ColorConsole.WriteLineProgram($"Deleting all users");
+      await _mediator.Send(new DeleteAllUsersCommand());
+      ColorConsole.WriteLineProgram($"All users deleted");
+      
+      Console.ReadLine();
+
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using CQRSwithMediatoR.Infrastructure;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CQRSwithMediatoR
+{
+  public class Program
+  {
+    static async Task Main(string[] args)
+    {
+      Console.WriteLine("Legend:");
+
+        ColorConsole.WriteLineCommandHandler("Command - Handlers are blue");
+        ColorConsole.WriteLineQueryHandler("Query - Handlers are yellow");
+        ColorConsole.WriteLineProgram("Program code is green");
+        ColorConsole.WriteLineRepository("Repository code is magenta");
+
+        var serviceProvider = CreateServiceProvider();
+        await serviceProvider.GetRequiredService<Execution>().RunAsync();
+
+        ServiceProvider CreateServiceProvider()
+        {
+          var collection = new ServiceCollection();
+          //This will execute our main logic.
+          collection.AddScoped<Execution>();
+
+          // This comes from the MediatR package.
+          // It looks for all commands, queries and handlers and registers
+          // them in the container
+          //collection.AddMediatR(typeof(Program).Assembly);
+          collection.AddMediatR(typeof(Program).Assembly);
+
+          // Our repository
+          collection.AddScoped<UserRepository>();
+
+          return collection.BuildServiceProvider();
+        }
+    }
+  }
+}
